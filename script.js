c96c164993f43ad09643bb0b933598a252eab4b4
amjad-salha <amjadsalha319@gmail.com>
2025-09-20 23:29:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 document.getElementById('contactForm').onsubmit = function(e) {
   e.preventDefault();
-  document.getElementById('formMessage').innerText = 'تم إرسال رسالتك بنجاح! سنرد عليك قريباً.';
+  let name = this.name.value.trim();
+  let email = this.email.value.trim();
+  let msg = this.message.value.trim();
+  let msgDiv = document.getElementById('formMessage');
+  if (name.length < 2) {
+    msgDiv.innerText = "يرجى كتابة الاسم بشكل صحيح.";
+    msgDiv.style.color = "#d32626";
+    return;
+  }
+  if (!email.match(/^[\w\.-]+@[\w\.-]+\.\w{2,4}$/)) {
+    msgDiv.innerText = "يرجى إدخال بريد إلكتروني صحيح.";
+    msgDiv.style.color = "#d32626";
+    return;
+  }
+  if (msg.length < 5) {
+    msgDiv.innerText = "يرجى كتابة رسالة واضحة.";
+    msgDiv.style.color = "#d32626";
+    return;
+  }
+  msgDiv.innerText = 'تم إرسال رسالتك بنجاح! سنرد عليك قريباً.';
+  msgDiv.style.color = "#389e3d";
   this.reset();
 };
